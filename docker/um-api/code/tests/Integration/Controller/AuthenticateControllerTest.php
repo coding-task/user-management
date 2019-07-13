67a3bb1fae16a4b4a0d01db3fcf1aaae3c09df51
Rajendra Sharma <drudge.rajan@gmail.com>
2019-07-13 13:11:16 +0700

    test setup

---
+++ docker/um-api/code/tests/Integration/Controller/AuthenticateControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Integration\Controller;
+
+use Tests\IntegrationTestCase;
+
+class AuthenticateControllerTest extends IntegrationTestCase
+{
+    /**
+     * @test
+     *
+     * @covers ::__construct
+     * @covers ::authenticate
+     */
+    public function it_throws_validation_exception_for_invalid_request()
+    {
+        $user = $this->post('/users/authenticate', [
+            'email' => 'superadmin@admin.com',
+        ]);
+
+        $response = json_decode($user->response->getContent(), true);
+
+        $this->assertResponseStatus(422);
+        $this->assertEquals([
+            'errors' => [
+                'password' => [
+                    'The password field is required.',
+                ],
+            ],
+        ], $response);
+    }
+    /**
+     * @test
+     *
+     * @covers ::authenticate
+     */
+    public function it_fails_authentication_for_invalid_user()
+    {
+        $this->post('/users/authenticate', [
+            'email' => 'invalid@invalid.com',
+            'password' => 'invalid',
+        ]);
+        $this->assertResponseStatus(500);
+    }
+    /**
+     * @test
+     *
+     * @covers ::authenticate
+     */
+    public function it_authenticates_user()
+    {
+        $user = $this->post('/users/authenticate', [
+            'email' => 'superadmin@admin.com',
+            'password' => 'password@123',
+        ]);
+        $response = json_decode($user->response->getContent(), true);
+        $this->assertResponseOk();
+        $this->assertNotEmpty($response['data']['token']);
+    }
+}
---
+++ docker/um-api/code/tests/IntegrationTestCase.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests;
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class IntegrationTestCase extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Set up.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->runDatabaseMigrations();
+        $this->artisan('db:seed');
+    }
+
+    /**
+     * Tear Down.
+     */
+    public function tearDown(): void
+    {
+        $this->beforeApplicationDestroyed(function () {
+            app('db')->connection()->disconnect();
+        });
+
+        parent::tearDown();
+    }
+
+    /**
+     * Get admin user.
+     *
+     * @return string|null
+     */
+    public function getAdminUser()
+    {
+        $admin = $this->post($this->baseUrl . '/users/authenticate', [
+            'email' => 'superadmin@admin.com',
+            'password' => 'password@123',
+        ]);
+
+        $response = json_decode($admin->response->getContent(), true);
+
+        return $response['data']['token'] ?? null;
+
+    }
+}
